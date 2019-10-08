5ee0f47c28000e7af82b110681aa5c807335ebbb
cindyl5697 <54374245+cindyl5697@users.noreply.github.com>
2019-10-08 11:44:36 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"
-let userName = "YourUsername";
+let ownerName = "Cindy Li"
+let userName = "Cindyl5697";
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
